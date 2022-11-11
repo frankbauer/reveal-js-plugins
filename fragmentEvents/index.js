01cb7af1ea1f99dee61fa8fa209735dd233dbcab
Frank <frank.bauer@fau.de>
2022-11-11 16:06:28 +0100

    Moved to Stylus

--- fragmentEvents/index.js
+++ fragmentEvents/index.js
@@ -1,86 +1,96 @@
-var FragmentEvents = () => {
+window.FragmentEvents = () => {
 
-	// The reveal.js instance this plugin is attached to
-	let reveal;	
+    // The reveal.js instance this plugin is attached to
+    let reveal
 
-	
+    function addStyleSheet(name){
+        const head = document.head || document.getElementsByTagName('head')[0],
+            link = document.createElement('link')
 
-    function eventProcessor(event){
-		const slide = reveal.getCurrentSlide();
-		const idx = reveal.getState().indexf;
-		const fragmentState = slide.hasAttribute('data-fragment-event-state')?+slide.getAttribute('data-fragment-event-state'):-2;
-		
-		const showFragments = fragmentState<=idx				
-		
-		const fragmentIndices = []
-		const fragmentIndicesHide = []
-		if (showFragments){
-			for (let i=fragmentState+1; i<=idx; i++){
-				fragmentIndices.push(i);
-			}												
-		} else {
-			fragmentIndices.push(idx)
-			for (let i=fragmentState; i>idx; i--){
-				fragmentIndicesHide.push(i);
-			}
-		}
-		const data = {
-			getFragment:(idx) => slide.querySelector(`[data-fragment-index="${idx}"]`),
-			getFragments:(idx) => slide.querySelectorAll(`[data-fragment-index="${idx}"]`),
-			setData:(name, data) => slide.setAttribute(`data-fe-${name}`, data),
-			getData:(name) => slide.getAttribute(`data-fe-${name}`),
-			hasData:(name) => slide.hasAttribute(`data-fe-${name}`),
-			slide:slide
-		}
-
-		console.log(`idx: ${idx}, last:${fragmentState}, show:${fragmentIndices}, hide:${fragmentIndicesHide}, attr:${slide.getAttribute('data-fragment-event-state')}`);		
-		if (fragmentState===-2){
-			reveal.dispatchEvent({
-				type:'fragmentevent_init',
-				data:data
-			})
-		}
-		if (fragmentIndices.length>0 || fragmentIndicesHide.length>0 ){
-			reveal.dispatchEvent({
-				type:'fragmentevent_change',
-				data:{
-					...data,
-					showFragments:fragmentIndices,
-					hideFragments:fragmentIndicesHide,
-					lastActiveFragment:fragmentState,
-					fragmentIndex:idx
-				}
-			})
-		}
-		
-		slide.setAttribute('data-fragment-event-state', idx)
+        link.rel  = 'stylesheet'
+        link.type = 'text/css'
+        link.href = `${window.ModelViewerPluginFile}/${name}.css`
+        link.media = 'all'
+        head.appendChild(link)
     }
 
-	function convertFragmentContainers(){
-		//Have a simplified Fragment container Controller Stage
-		const fragments = document.getElementsByTagName('Fragment')
-		for (const el of fragments) {
-			el.classList.add("fragment")
-		}
-	}
+    function eventProcessor(event){
+        const slide = reveal.getCurrentSlide()
+        const idx = reveal.getState().indexf
+        const fragmentState = slide.hasAttribute('data-fragment-event-state')?+slide.getAttribute('data-fragment-event-state'):-2
 
-	return {
-		id: 'fragmentevents',
+        const showFragments = fragmentState<=idx
 
-		/**
+        const fragmentIndices = []
+        const fragmentIndicesHide = []
+        if (showFragments){
+            for (let i=fragmentState+1; i<=idx; i++){
+                fragmentIndices.push(i)
+            }
+        } else {
+            fragmentIndices.push(idx)
+            for (let i=fragmentState; i>idx; i--){
+                fragmentIndicesHide.push(i)
+            }
+        }
+        const data = {
+            getFragment:(idx) => slide.querySelector(`[data-fragment-index="${idx}"]`),
+            getFragments:(idx) => slide.querySelectorAll(`[data-fragment-index="${idx}"]`),
+            setData:(name, data) => slide.setAttribute(`data-fe-${name}`, data),
+            getData:(name) => slide.getAttribute(`data-fe-${name}`),
+            hasData:(name) => slide.hasAttribute(`data-fe-${name}`),
+            slide:slide
+        }
+
+        console.log(`idx: ${idx}, last:${fragmentState}, show:${fragmentIndices}, hide:${fragmentIndicesHide}, attr:${slide.getAttribute('data-fragment-event-state')}`)
+        if (fragmentState===-2){
+            reveal.dispatchEvent({
+                type:'fragmentevent_init',
+                data:data
+            })
+        }
+        if (fragmentIndices.length>0 || fragmentIndicesHide.length>0 ){
+            reveal.dispatchEvent({
+                type:'fragmentevent_change',
+                data:{
+                    ...data,
+                    showFragments:fragmentIndices,
+                    hideFragments:fragmentIndicesHide,
+                    lastActiveFragment:fragmentState,
+                    fragmentIndex:idx
+                }
+            })
+        }
+
+        slide.setAttribute('data-fragment-event-state', idx)
+    }
+
+    function convertFragmentContainers(){
+        addStyleSheet('main')
+        //Have a simplified Fragment container Controller Stage
+        const fragments = document.getElementsByTagName('Fragment')
+        for (const el of fragments) {
+            el.classList.add('fragment')
+        }
+    }
+
+    return {
+        id: 'fragmentevents',
+
+        /**
 		 * Starts processing and converting Markdown within the
 		 * current reveal.js deck.
 		 */
-		init: function( revealIn ) {
-			reveal = revealIn;
-			
-			convertFragmentContainers();
+        init: function( revealIn ) {
+            reveal = revealIn
 
-            revealIn.on( 'ready', eventProcessor );
-			revealIn.on( 'slidechanged', eventProcessor);
-			revealIn.on( 'fragmentshown', eventProcessor);
-			revealIn.on( 'fragmenthidden', eventProcessor );
-		}
-	}
+            convertFragmentContainers()
 
-};
+            revealIn.on( 'ready', eventProcessor )
+            revealIn.on( 'slidechanged', eventProcessor)
+            revealIn.on( 'fragmentshown', eventProcessor)
+            revealIn.on( 'fragmenthidden', eventProcessor )
+        }
+    }
+
+}
